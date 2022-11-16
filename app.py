132e87db798e5d3f82a6dea396cbb8e704a31cb1
dknyz <dknyzzz777@gmail.com>
2022-11-16 11:15:31 +0800

    refactor : Update the sql query and Add nextPage null relationl operator

--- app.py
+++ app.py
@@ -34,60 +34,45 @@ def attractions():
 	try:
 		# page 起始頁為 0，代表 nextPage 起始頁為 1
 		page = int(request.args.get("page", None))
+		keyword = request.args.get("keyword", None)
 		nextPage = page+1
 		mycursor = mydb.cursor()
-		sql = "select * from attraction where id between %s and %s"
-		if nextPage == 1:
-			value = (nextPage, nextPage*12)
-			mycursor.execute(sql, value)
-			results = mycursor.fetchall()
-			# attraction 的 list
-			data = []
-			for list in results:
-				# 裝 data_item 的 dic
-				data_item = {
-					"id":list[0],
-					"name":list[1],
-					"category":list[2],
-					"description":list[3],
-					"address":list[4],
-					"transport":list[5],
-					"mrt":list[6],
-					"lat":list[7],
-					"lng":list[8],
-					"images":list[9].split(",")
-				}
-				print(list[9].split(","))
-				data.append(data_item)
-			return jsonify({
-				"nextPage":nextPage,
-				"data":data
-			}), 200
-		else:
-			value = (page*12, nextPage*12)
-			mycursor.execute(sql, value)
-			results = mycursor.fetchall()
-			# attraction 的 list
-			data = []
-			for list in results:
-				# 裝 data_item 的 dic
-				data_item = {
-					"id":list[0],
-					"name":list[1],
-					"category":list[2],
-					"description":list[3],
-					"address":list[4],
-					"transport":list[5],
-					"mrt":list[6],
-					"lat":list[7],
-					"lng":list[8],
-					"images":list[9]
-				}
-				data.append(data_item)
-			return jsonify({
-				"nextPage":nextPage,
-				"data":data
-			}), 200
+		# if nextPage == 1:
+		sql = "select * from attraction limit %s, %s "
+		value = (page*12, 13)
+		mycursor.execute(sql, value)
+		# results 為一個由資料庫回傳的 tuple
+		results = mycursor.fetchall()
+		print(len(results))
+		# attraction 的 list
+		data = []
+		round = 0
+		for list in results:
+			# 讀取 tuple 資料裝進 dic
+			data_item = {
+				"id":list[0],
+				"name":list[1],
+				"category":list[2],
+				"description":list[3],
+				"address":list[4],
+				"transport":list[5],
+				"mrt":list[6],
+				"lat":list[7],
+				"lng":list[8],
+				"images":list[9].split(",")
+			}
+			data.append(data_item)
+			# 計算次數，因為從資料庫取13筆資料，但只想放入12筆資料
+			round += 1
+			if round == 11 :
+				break
+		# 回傳前要確認 nextPage ， 如果從資料庫回傳的資料筆數小於13，代表下一頁沒有資料
+		if len(results) < 13 :
+			nextPage = None
+		return jsonify({
+			"nextPage":nextPage,
+			"data":data
+		}), 200
 	except Exception:
 		print("我有經過這裡")
 		return jsonify({
