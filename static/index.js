67ec558fd63e25e9cb728542c4c099c4938868a9
dknyz <dknyzzz777@gmail.com>
2022-11-24 22:25:16 +0800

    feat : Add isloading avoiding fetch twice before render on html; Add function categories, layerNone, rebuild remove for isloading; Rebuild function fetchPagefor keyword search; Add infinite scroll.

--- static/index.js
+++ static/index.js
@@ -1,12 +1,16 @@
+// isloading for infinite scroll
+let isloading = false;
+
 // HTML attractions section
 const attractionsContainer = document.querySelector("#grid-attractions-container");
 
 // 預設為 0
-var nextPage = 0
+var nextPage = [0];
 
 // 存放attractions的 name、mrt、category、images(只要第一個 url)
 let attractionsPool = [];
 
+/* 渲染attraction section HTML函式 */
 function insertImg(attractionsPool, dataNum){
     /* grid-attractions-item */ 
     let container = document.createElement('div');
@@ -42,17 +46,93 @@ function insertImg(attractionsPool, dataNum){
     wrap.append(img, name, mrtCategory);
     container.appendChild(wrap);
     attractionsContainer.appendChild(container);
-}
+};
+
+function categories(){
+    /* 打開搜尋分類框 */
+    let categories = document.querySelector("#slogan-search-categories")
+    categories.style.display = "grid";
+    /* 清空搜尋分類框，否則會留存上一次append進來的分類 */
+    categories.innerHTML = "";
+    /* 開啟分類框的隱形遮罩 */
+    let htmlLayer = document.querySelector("#htmlLayer-zindex50");
+    htmlLayer.style.display = "block";
+    /* fetch 資料並放入資料 */
+    fetch("/api/categories")
+    .then(function(response){
+        return response.json();
+    }).then(function(data){
+        let datalength = Object.keys(data['data']).length;
+        for(let dataNum = 0; dataNum < datalength; dataNum++){
+            let categoryItemDiv = document.createElement("div");
+            categoryItemDiv.setAttribute("id", "slogan-search-categories-item");
+            categoryItemDiv.textContent = data['data'][dataNum];
+            categoryItemDiv.addEventListener("click", function (){
+                let categoryInputValue = document.querySelector("#slogan-search-bar");
+                categoryInputValue.value = this.textContent;
+            })
+            let itemsContainer = document.querySelector("#slogan-search-categories");
+            itemsContainer.appendChild(categoryItemDiv); 
+        }
+    })
+};
+
+// function show(this){
+//     document.querySelector(this).style.display = "block";
+// }
+
+function layerNone(){
+    /* 關閉分類框的隱形遮罩 */
+    let htmlLayer = document.querySelector("#htmlLayer-zindex50");
+    htmlLayer.style.display = "none";
+    /* 關閉搜尋分類框 */
+    let categories = document.querySelector("#slogan-search-categories")
+    categories.style.display = "none";
+
+};
+
+/* 刪除 attractions 中的資料 */
+function remove(){
+    /* 點擊搜尋btn時排除無法讀取景點資料的可能性 */
+    isloading = false; 
+    /* 將下一頁歸零 */
+    nextPage = [];
+    nextPage.push(0);
+    console.log("remove's loading Page: "+ nextPage);
+    let first = document.querySelector("#grid-attractions-container").firstElementChild
+    while (first) {
+        first.remove();
+        first = document.querySelector("#grid-attractions-container").firstElementChild
+    }
+};
 
 
-function getPage(){
+/* 拿取頁面資料*/
+function fetchPage(){
+    /* 標記工作階段 */
+    if(isloading == false){
+        isloading = true;
+    }else{
+        return
+    }
+    console.log("fetchPage's loading Page: "+nextPage);
+    /* 取得關鍵字搜尋 */
+    let keyword = document.querySelector("#slogan-search-bar").value;
+    console.log("keyword: " + keyword + "typeof: " + typeof keyword + "keyword-length: " + keyword.length);
+    if(keyword.length == 0){
+        url = `/api/attractions?page=${nextPage[0]}`
+    }else{
+        url = `/api/attractions?page=${nextPage[0]}&keyword=${keyword}`
+    };
     /* 第一次載入時的 nextPage = 0 */
-    fetch(`/api/attractions?page=${nextPage}`)
+    fetch(url)
     .then(function(response){
         return response.json(); // 讀取為 JSON 格式
     }).then(function(data){
         /* 替換nextPage (type = var)非 let */
-        nextPage = data['nextPage'];
+        nextPage = [];
+        nextPage.push(data['nextPage']);
+        console.log("nextPage: " + nextPage);
         /* 取得該頁有幾筆資料 */
         let datalength = Object.keys(data['data']).length;
         for(let dataNum = 0; dataNum < datalength ;dataNum++){
@@ -62,11 +142,39 @@ function getPage(){
                 data['data'][dataNum]['category'], 
                 data['data'][dataNum]['images'][0]
             ])
-        }
+        };
         for(dataNum = 0; dataNum < datalength; dataNum++){
             insertImg(attractionsPool, dataNum);
-        }
+        };
         /* 清空，讓下一頁資料填進來 */
-        attractionsPool = []
+        attractionsPool = [];
+        isloading = false
     })
-}
+};
+
+/* 無限捲軸 */
+
+/* 創造 Intersection Observer */
+const options = {
+    root : document.querySelector('flex-col'),
+    rootMargin: '10px',
+    threshold: 1,
+};
+
+/* callback 函式 */
+const callback = ([entry]) => {
+    if (entry && entry.isIntersecting && isloading == false){
+        fetchPage();
+        console.log("observer");
+        console.log(entry)
+    }
+};
+
+/* 創建一個 observer */
+const observer = new IntersectionObserver(callback, options);
+
+/* 觀察 target */
+const target = document.querySelector('footer');
+observer.observe(target);
+
+/* 每回呼完我就關閉觀察對象，進到fetch函式後我再打開觀察對象 */
