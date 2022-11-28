993929310bcae8fdacb40fdd08082c217f46372b
dknyz <dknyzzz777@gmail.com>
2022-11-29 00:16:04 +0800

    feat : Add citation in data.py.

--- data/data.py
+++ data/data.py
@@ -8,26 +8,27 @@ mydb = mysql.connector.connect (
   password = getPassword(),
   database = "taipei_day_trip"
 )
-with open('data/taipei-attractions.json', mode='r', encoding="utf-8") as file:
+with open('taipei-attractions.json', mode='r', encoding="utf-8") as file:
   data = json.load(file)
 results = data["result"]["results"]
-i=0
+id=-1 # 就會從位置0開始跑，不會多跑一筆
 mycursor = mydb.cursor()
 try:
   for data in results :
     url_files = []
-    print(i)
-    i +=1
-    urls = data["file"]
-    matchedResult = ["https:"+i for i in re.split(r"https:", urls)] # 整理成拆開的 url list
+    id +=1
+    urls = data["file"] # 圖片檔的url字串(全黏在一起)
+    # 利用正規表達式將url分開並放進串列(list)
+    matchedResult = ["https:"+ url for url in re.split(r"https:", urls)] # 整理成拆開的 url list
+    print(f"{id}: {matchedResult}")
+    # 留下.jpg or .JPG 圖檔
     for url in matchedResult:
       if url.endswith(".jpg") == True or url.endswith(".JPG") == True: # 去除非圖片檔的 url
         url_files.append(url)
-    print(url_files)
+    # 將資料放入MySQL資料庫中
     sql = "insert into attraction (name, category, description, address, transport, mrt, lat, lng, images) values(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    value = (results[i]["name"], results[i]["CAT"], results[i]["description"], results[i]["address"], results[i]["direction"], results[i]["MRT"], results[i]["latitude"], results[i]["longitude"], ','.join(url_files))
+    value = (results[id]["name"], results[id]["CAT"], results[id]["description"], results[id]["address"], results[id]["direction"], results[id]["MRT"], results[id]["latitude"], results[id]["longitude"], ','.join(url_files))
     mycursor.execute(sql, value)
     mydb.commit()
 finally:
-  print("mycursor_close")
   mycursor.close()
