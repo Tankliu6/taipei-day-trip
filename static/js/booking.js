6f8ea6d1c527b5e1b7c467724a9d0fbf75d242ec
dknyz <dknyzzz777@gmail.com>
2022-12-22 00:23:46 +0800

    feat : Add tappay third-party payment gateway suite in booking.css, booking.js and booking.html.

--- static/js/booking.js
+++ static/js/booking.js
@@ -14,15 +14,13 @@ function checkMemberStatus(){
                 if(data.data.attractions.length === 0 ){
                     checkHaveBookingOrNot();
                 };
-                const welcomeName = document.querySelector("#welcome-name");
-                welcomeName.textContent = data.name;
-                const contactName = document.querySelector("#contact-name");
-                contactName.value = data.name;
-                const contactEmail = document.querySelector("#contact-email");
-                contactEmail.value = data.email;
-                for(bookingInfo of data.data.attractions){
+                renderBookingBasicInfo(data);
+                let totalCostToPay = 0;
+                data.data.attractions.forEach(bookingInfo => {
                     renderItemsSection(bookingInfo);
-                };
+                    totalCostToPay += Number(bookingInfo.price)
+                });
+                renderTotalCostToPay(totalCostToPay);
             })
         };
     })
@@ -35,12 +33,21 @@ function checkHaveBookingOrNot(){
     const cleanTheBooking = document.querySelector(".wrap-content-forRWD");
     const sectionForNoBooking = document.querySelector("#noBooking");
     const goToBooking = document.querySelector(".goToBooking");
-    if(countSection <= 6){
+    if(countSection <= 7){
         cleanTheBooking.style.display = "none";
         sectionForNoBooking.style.display = "block";
         goToBooking.href = `${window.location.origin}/`
     }
 };
+
+function renderBookingBasicInfo(data){
+    const welcomeName = document.querySelector("#welcome-name");
+    welcomeName.textContent = data.name;
+    const contactName = document.querySelector("#contact-name");
+    contactName.value = data.name;
+    const contactEmail = document.querySelector("#contact-email");
+    contactEmail.value = data.email;    
+}
 
 function renderItemsSection(bookingInfo){
     let time;
@@ -76,6 +83,11 @@ function renderItemsSection(bookingInfo){
     removeBookingBtn.addEventListener("click", removeBooking)
 }
 
+function renderTotalCostToPay(totalCostToPay){
+    const totalCostShowToUser = document.querySelector(".total-cost-in-dollars");
+    totalCostShowToUser.textContent = totalCostToPay;
+}
+
 function removeBooking(){
     const bookingWantToDelete = this.value;
     let responseStatus;
@@ -93,11 +105,220 @@ function removeBooking(){
         return response.json();
     })
     .then(function(data){
-        console.log(data);
         window.location.reload();
     })
 }
 
+/* tappay金流套件 */
+
+// Display ccv field
+let fields = {
+    number: {
+        // css selector
+        element: '#card-number',
+        placeholder: '**** **** **** ****'
+    },
+    expirationDate: {
+        // DOM object
+        element: document.getElementById('card-expiration-date'),
+        placeholder: 'MM / YY'
+    },
+    ccv: {
+        element: '#card-ccv',
+        placeholder: 'ccv'
+    }
+}
+
+TPDirect.card.setup({    
+    fields: fields,
+    styles: {
+        // Style all elements
+        'input': {
+            'color': 'gray'
+        },
+        // Styling ccv field
+        'input.ccv': {
+            // 'font-size': '16px'
+        },
+        // Styling expiration-date field
+        'input.expiration-date': {
+            // 'font-size': '16px'
+        },
+        // Styling card-number field
+        'input.card-number': {
+            // 'font-size': '16px'
+        },
+        // style focus state
+        ':focus': {
+            // 'color': 'black'
+        },
+        // style valid state
+        '.valid': {
+            'color': 'green'
+        },
+        // style invalid state
+        '.invalid': {
+            'color': 'red'
+        },
+        // Media queries
+        // Note that these apply to the iframe, not the root window.
+        '@media screen and (max-width: 400px)': {
+            'input': {
+                'color': 'orange'
+            }
+        }
+    },
+    // 此設定會顯示卡號輸入正確後，會顯示前六後四碼信用卡卡號
+    isMaskCreditCardNumber: true,
+    maskCreditCardNumberRange: {
+        beginIndex: 6,
+        endIndex: 11
+    }
+})
+
+TPDirect.card.onUpdate(function (update) {
+    // update.canGetPrime === true
+    // --> you can call TPDirect.card.getPrime()
+    if (update.canGetPrime) {
+        // Enable submit Button to get prime.
+        // submitButton.removeAttribute('disabled')
+    } else {
+        // Disable submit Button to get prime.
+        // submitButton.setAttribute('disabled', true)
+    }
+
+    // cardTypes = ['mastercard', 'visa', 'jcb', 'amex', 'unionpay','unknown']
+    if (update.cardType === 'visa') {
+        // Handle card type visa.
+    }
+
+    // number 欄位是錯誤的
+    if (update.status.number === 2) {
+        // setNumberFormGroupToError()
+    } else if (update.status.number === 0) {
+        // setNumberFormGroupToSuccess()
+    } else {
+        // setNumberFormGroupToNormal()
+    }
+
+    if (update.status.expiry === 2) {
+        // setNumberFormGroupToError()
+    } else if (update.status.expiry === 0) {
+        // setNumberFormGroupToSuccess()
+    } else {
+        // setNumberFormGroupToNormal()
+    }
+
+    if (update.status.ccv === 2) {
+        // setNumberFormGroupToError()
+    } else if (update.status.ccv === 0) {
+        // setNumberFormGroupToSuccess()
+    } else {
+        // setNumberFormGroupToNormal()
+    }
+})
+
+/* request data for orders */
+function processOrdersData(orderDataFromPage){
+    const totalCostInDollars = document.querySelector(".total-cost-in-dollars").textContent;
+    orderDataFromPage.totalPrice = totalCostInDollars;
+    const orders = document.querySelectorAll("#items");
+    const regex = /預定編號 : (\d+)/;
+    orders.forEach(eachOrder => {
+        const bookingIdElement = eachOrder.querySelector("#booking-id");
+        const match = bookingIdElement.textContent.match(regex);
+        if (match) {
+            const bookingId = match[1];
+            orderDataFromPage.bookingId.push(bookingId);
+        }
+    });
+    return orderDataFromPage
+}
+
+function contactInfo(){
+    const contactName = document.querySelector("#contact-name").value;
+    const contactEmail = document.querySelector("#contact-email").value;
+    const contactPhone = document.querySelector("#contact-phone").value;
+    return {
+            name : contactName,
+            email : contactEmail,
+            phone : contactPhone
+    }
+}
+
+// call TPDirect.card.getPrime when user submit form to get tappay prime
+const checkOrderToPayButton = document.querySelector(".check-order-to-pay");
+checkOrderToPayButton.addEventListener("click", onSubmit);
+function onSubmit(event) {
+    event.preventDefault()
+
+    // 取得 TapPay Fields 的 status
+    const tappayStatus = TPDirect.card.getTappayFieldsStatus()
+
+    // 確認是否可以 getPrime
+    if (tappayStatus.canGetPrime === false) {
+        alert('can not get prime')
+        return
+    }
+
+    // Get prime
+    TPDirect.card.getPrime((result) => {
+        if (result.status !== 0) {
+            // alert('get prime error ' + result.msg)
+            return
+        }
+        const prime = result.card.prime;
+        const orderDataFromPage = {
+            "totalPrice" : "",
+            "bookingId" : []
+        };
+        const bookingIdAndTotalCost = processOrdersData(orderDataFromPage);
+        const contact = contactInfo();
+        loadingStart();
+        let responseStatus;
+        fetch("/api/orders", {
+            method : "POST",
+            credentials : "include",
+            body : JSON.stringify(
+                {
+                    "prime" : prime,
+                    "order" : bookingIdAndTotalCost,
+                    "contact" : contact
+                }
+            ),
+            cache : "no-cache",
+            headers : new Headers({
+                "content-type" : "application/json"
+            })
+        }).then(function(response){
+            responseStatus = response.status;
+            return response.json()
+        }).then(function(orderData){
+            if(responseStatus === 200){
+                const orderNumber = orderData.data.orderNumber;
+                window.location.assign(`${window.location.origin}/thankyou?number=${orderNumber}`)
+            }
+            else{
+                loadingComplete();
+                popUpMessage(`${orderData.message}`)
+            }
+        })
+
+        // alert('get prime 成功，prime: ' + result.card.prime)
+        // send prime to your server, to pay with Pay by Prime API .
+        // Pay By Prime Docs: https://docs.tappaysdk.com/tutorial/zh/back.html#pay-by-prime-api
+    })
+}
+
+function loadingStart(){
+    const loadingLayer = document.querySelector(".layer-payment-loading");
+    loadingLayer.style.display = "block";
+}
+
+function loadingComplete(){
+    const loadingLayer = document.querySelector(".layer-payment-loading");
+    loadingLayer.style.display = "none";
+}
 
 /* 載入時啟動 */
 checkMemberStatus();
