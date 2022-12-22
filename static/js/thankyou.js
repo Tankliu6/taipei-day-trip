919efc4da97e868dff8b1399adb5616c2d89afe9
dknyz <dknyzzz777@gmail.com>
2022-12-22 10:28:00 +0800

    feat : Add check Member-Status function avoiding the user logout in thank-you page in thankyou.js.

--- static/js/thankyou.js
+++ static/js/thankyou.js
@@ -1,3 +1,22 @@
+function checkMemberStatus(){
+    fetch("/api/user/auth", {
+        method : "GET",
+        credentials: "include",
+        cache: "no-cache",
+        headers: new Headers({
+            "content-type": "application/json"
+        })
+    }).then(function(response){
+        return response.json();
+    }).then(function(responseJsonData){
+        if(responseJsonData.data != null){
+            return
+        }else{
+            window.location.assign(`${window.location.origin}/`)            
+        };
+    });
+};
+
 function getOrderNumber(){
     const orderNumber = document.querySelector(".order-number");
     orderNumberFromUrl = window.location.search.split("=")[1];
@@ -6,3 +25,4 @@ function getOrderNumber(){
 
 /* 載入時觸發 */
 getOrderNumber();
+checkMemberStatus();
