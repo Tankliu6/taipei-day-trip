6791511bbede007f5c0cca43d0013e233557d422
dknyz <dknyzzz777@gmail.com>
2022-12-22 14:45:03 +0800

    feat : Advance the payment process in api_order.py.

--- controller/api_order.py
+++ controller/api_order.py
@@ -20,19 +20,20 @@ def checkLoginStatus():
     else:
         return False
 
-def processOrderStatus(orderNumber):
+def processOrderStatus(orderNumberData):
     try:
         cookieMemberInfo = request.cookies.get("Set-Cookie")
         memberInfo = decode_token(cookieMemberInfo)
         bookingData = request.get_json()
         cnx = cnxpool.get_connection()
 
+        orderDatetime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        orderNumber = f"{memberInfo['id']}-{orderDatetime}"
         mycursor1 = cnx.cursor()
-        sql = "INSERT INTO orders (user_id, booking_id, attraction_id, date, time, price, order_status, order_date, contact_name, contact_email, contact_phone) SELECT user_id, id, attraction_id, date, time, price, '1', NOW(), %s, %s, %s FROM booking WHERE user_id = %s;"
-        value1 = (bookingData["contact"]["name"], bookingData["contact"]["email"], bookingData["contact"]["phone"], memberInfo["id"])
+        sql = "INSERT INTO orders (user_id, order_id, attraction_id, date, time, price, order_status, order_date, contact_name, contact_email, contact_phone) SELECT user_id, %s, attraction_id, date, time, price, '1', NOW(), %s, %s, %s FROM booking WHERE user_id = %s;"
+        value1 = (orderNumber, bookingData["contact"]["name"], bookingData["contact"]["email"], bookingData["contact"]["phone"], memberInfo["id"])
         mycursor1.execute(sql, value1)
         cnx.commit()
-        mycursor1.close()
 
         sql2 = "DELETE FROM booking WHERE user_id = %s"
         value2 = (memberInfo["id"], )
@@ -64,54 +65,37 @@ def processOrderStatus(orderNumber):
         # responseFromTappay = await asyncio.wait_for(requests.post(tappayUrl, json=payload, headers=headers), timeout=90)       
         responseFromTappay = requests.post(tappayUrl, json = payload, headers = headers)
         order_status = responseFromTappay.json()["status"]
-
         if order_status == 1:
-            mycursor3 = cnx.cursor()
-            sql3 = "INSERT INTO history (user_id, date, price, status, booking_id) values(%s, NOW(), %s, %s, %s)"
-            value3 = (memberInfo["id"], bookingData["order"]["totalPrice"], order_status, json.dumps(bookingData["order"]["bookingId"]))
-            mycursor3.execute(sql3, value3)
-            cnx.commit()
-            mycursor3.close()
-            orderNumber["message"] = "付款失敗"
+            orderNumberData["orderNumber"] = orderNumber
+            orderNumberData["orderStatus"] = order_status
+            orderNumberData["message"] = "訂單付款失敗"
             return True
 
         elif order_status == 0:
             mycursor3 = cnx.cursor()
-            sql3 = "INSERT INTO history (user_id, date, price, status, booking_id) values(%s, NOW(), %s, %s, %s)"
-            value3 = (memberInfo["id"], bookingData["order"]["totalPrice"], order_status, json.dumps(bookingData["order"]["bookingId"]))
+            sql3 = "update orders set order_status = %s where order_id = %s"
+            value3 = (order_status, orderNumber)
             mycursor3.execute(sql3, value3)
             cnx.commit()
-            mycursor3.close()
 
-            mycursor4 = cnx.cursor(dictionary=True)
-            sql4 = "SELECT id, booking_id, date FROM history WHERE user_id = %s ORDER BY date DESC LIMIT 1"
-            value4 = (memberInfo["id"], )
-            mycursor4.execute(sql4, value4)
-            orderHistory = mycursor4.fetchall()
-            shoppingCartBookingId = tuple(int(x.strip('" ')) for x in orderHistory[0]["booking_id"].strip("[]''").split(','))
-
-            for booking_id in shoppingCartBookingId:
-                mycursor5 = cnx.cursor()
-                sql5 = "update orders set order_status = %s where booking_id = %s"
-                value5 = (order_status, booking_id)
-                mycursor5.execute(sql5, value5)
-                cnx.commit()
-                mycursor5.close()
-
-            order_date = orderHistory[0]["date"].strftime('%Y%m%d')
-            orderNumber["orderNumber"] = f"{orderHistory[0]['id']}TPDT{order_date}"
-            orderNumber["orderStatus"] = order_status
-            orderNumber["message"] = "付款成功，旅途愉快"
+            orderNumberData["orderNumber"] = orderNumber
+            orderNumberData["orderStatus"] = order_status
+            orderNumberData["message"] = "付款成功，旅途愉快"
             return True
+    except Exception as e:
+        print(e)
+        return False
     finally:
+        mycursor1.close()
         mycursor2.close()
+        mycursor3.close()
         cnx.close()
 
 
 @api_order.route("/api/orders", methods = ["POST"])
 def orders():
     try:
-        orderNumber = {
+        orderNumberData = {
             "orderNumber" : "",
             "orderStatus" : "",
             "message" : ""
@@ -122,22 +106,23 @@ def orders():
                     "error" : True,
                     "message" : "未登入系統，拒絕存取"
                 }, 403
-            elif processOrderStatus(orderNumber):
+            elif processOrderStatus(orderNumberData):
                 return {
                     "data" : {
-                        "orderNumber" : orderNumber["orderNumber"],
+                        "orderNumber" : orderNumberData["orderNumber"],
                         "payment" : {
-                            "status" : orderNumber["orderStatus"],
-                            "message" : orderNumber["message"]
+                            "status" : orderNumberData["orderStatus"],
+                            "message" : orderNumberData["message"]
                         }
                     }
                 }, 200    
             else:
                 return {
                     "error" : True,
-                    "message" : "訂單付款失敗，輸入不正確或其他原因"
+                    "message" : "訂單建立失敗，輸入不正確或其他原因"
                 }, 400
     except Exception as e:
+        print(e)
         return {
             "error" : True,
             "message" : "伺服器內部錯誤，請聯繫客服人員"
@@ -146,47 +131,43 @@ def orders():
 @api_order.route("/api/order/<orderNumber>", methods = ["GET"])
 def order(orderNumber):
     try:
-        historyOrderId = orderNumber.split("TPDT")[0]
         cnx = cnxpool.get_connection()
         mycursor1 = cnx.cursor(dictionary = True)
         if request.method == "GET":
             if checkLoginStatus() == True:
-                sql1 = "select * from history where id = %s"
-                value1 = (historyOrderId, )
+                sql1 = "select orders.*, attraction.name, attraction.address, attraction.images from orders inner join attraction on orders.attraction_id = attraction.id where order_id = %s"
+                value1 = (orderNumber, )
                 mycursor1.execute(sql1, value1)
-                orderInfoList = mycursor1.fetchone()
+                orderInfoList = mycursor1.fetchall()
                 trips = []
-                for orderId in json.loads(orderInfoList["booking_id"]):
-                    mycursor2 = cnx.cursor(dictionary = True)
-                    sql2 = "select orders.*, attraction.name, attraction.address, attraction.images from orders inner join attraction on orders.attraction_id = attraction.id where orders.booking_id = %s"
-                    value2 = (orderId,)
-                    mycursor2.execute(sql2, value2)
-                    trip = mycursor2.fetchone()
+                totalCost = {"totalCost" : 0}
+                for orderId in orderInfoList:
                     attraction = {
                         "attraction" : {
-                            "id" : trip["attraction_id"],
-                            "name" : trip["name"],
-                            "address" : trip["address"],
-                            "image" : trip["images"].split(",")[0]
+                            "id" : orderId["attraction_id"],
+                            "price" : orderId["price"],
+                            "name" : orderId["name"],
+                            "address" : orderId["address"],
+                            "image" : orderId["images"].split(",")[0]
                         },
-                        "date" : trip["date"],
-                        "time" : trip["time"]
+                        "date" : orderId["date"],
+                        "time" : orderId["time"]
                     }
                     contact = {
-                        "contact_name" : trip["contact_name"],
-                        "contact_email" : trip["contact_email"],
-                        "contact_phone" : trip["contact_phone"]
+                        "contact_name" : orderId["contact_name"],
+                        "contact_email" : orderId["contact_email"],
+                        "contact_phone" : orderId["contact_phone"]
                     }
+                    totalCost["totalCost"] += int(orderId["price"])
                     trips.append(attraction)
                     trips.append(contact)                                   
-                    mycursor2.close()
                 return {
                     "data" : {
                         "number" : orderNumber,
-                        "totalCost" : int(orderInfoList["price"]),
+                        "totalCost" : totalCost["totalCost"],
                         "trips" : trips,
                     },
-                    "status" : orderInfoList["status"]
+                    "status" : orderInfoList[0]["order_status"]
                 }, 200
             elif checkLoginStatus() != True:
                 return {
@@ -194,6 +175,7 @@ def order(orderNumber):
                     "message" : "未登入系統，拒絕存取"
                 }, 400
     except Exception as e:
+        print(e)
         return {
             "error" : True,
             "message" :"伺服器內部錯誤"
